b9e2f14eecf16423c31f64c686abe259a2a50612
RitaRez <ritarezborges@gmail.com>
2018-10-20 12:17:20 -0300

    Ta ficando cada vez mais daw

--- rijksmuseum/src/constants.js
+++ rijksmuseum/src/constants.js
@@ -1,2 +1,8 @@
-export const API_ENDPOINT = "www.rijksmuseum.nl/api/en/"
+export const API_ENDPOINT = "https://community-rijksmuseum.p.mashape.com/en/"
 export const API_KEY = "MFtrv9X3"
+
+export const CORS = {
+  'Access-Control-Allow-Origin' : '*',
+  'Access-Control-Allow-Methods' : 'POST, GET',
+  'Access-Control-Allow-Headers' : 'X-PINGOTHER, Content-Type'
+}
--- rijksmuseum/src/containers/ArtWork/index.js
+++ rijksmuseum/src/containers/ArtWork/index.js
@@ -7,32 +7,59 @@ class ArtWork extends React.Component { // eslint-disable-line react/prefer-stat
   constructor (props) {
     super(props)
     this.state = {
+      'objectNumber':this.props.match.params.id,
+      'title':'',
+      'image':'',
+      'description':'',
+      'painter':{
+        'name': '',
+        'placeOfBirth':'',
+        'dateOfBirth':'',
+        'placeOfDeath':'',
+        'dateOfDeath':''
+      },
+      'materials': [],
+      'dating':''
     }
-
   }
 
-  componentDidMount () {
+  componentWillMount () {
 
-    let status
     let request_url = API_ENDPOINT + 'collection/' + this.props.match.params.id+ '?key=' + API_KEY + '&format=json'
-
     let requestOptions = {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json'
+        "Accept": "text/plain",
+        "X-Mashape-Key": "mn4uddDIV8mshCmdALohqzK2ZHU9p1k87m8jsnMrL7onzILla2"
       }
     }
     fetch(request_url, requestOptions)
+      .then((response) => response.json())
+      .then((response) => response.artObject)
       .then(response => {
-        console.log(response)
-
+        if(response){
+          this.setState({
+            title: response.title,
+            image: response.webImage.url,
+            description: response.plaqueDescriptionEnglish,
+            painter:{
+              name: response.principalMakers[0].name,
+              placeOfBirth: response.principalMakers[0].placeOfBirth,
+              dateOfBirth: response.principalMakers[0].dateOfBirth,
+              placeOfDeath: response.principalMakers[0].placeOfDeath,
+              dateOfDeath: response.principalMakers[0].dateOfDeath
+            }
+          })
+        } else {
+          console.log('nem existe');
+        }
       })
       .catch((err) => {
         console.log(err)
       })
 
   }
-  
+
   render () {
     return (
       <div></div>

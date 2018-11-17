f48d80c6d3b7c7b2443637de280a2f69733b0a74
Bernardo Nunes <nunesbernardo2000@gmail.com>
2018-11-17 18:29:24 -0200

    filtragem de personagens por casa e livro está funcionado

--- app.js
+++ app.js
@@ -46,10 +46,11 @@ app.factory('InfoService', function($http){
 
 
 /* Controller */
-app.controller('Controller', ['InfoService', function(service){
+app.controller('Controller', ['$scope', 'InfoService', function($scope, service){
   var self = this;
   self.characters = [];
   self.character = {};
+  self.allCharacters = [];
     
   /* Família do personagem */
   self.father = {};
@@ -74,12 +75,13 @@ app.controller('Controller', ['InfoService', function(service){
     while (page < 200){ // Recupera todas os personagens
         service.getMultiple('characters', page, function(answer) {
             if (answer !== null) {
-                self.characters = self.characters.concat(answer);
+                self.allCharacters = self.allCharacters.concat(answer);
+                self.characters = self.allCharacters;
             }
         });
         page++;
-
     }
+
 
     page = 1;
     while (page < 200){
@@ -173,5 +175,32 @@ app.controller('Controller', ['InfoService', function(service){
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
+
+    $scope.filterByHouse = function(house){
+        self.characters = [];
+        for (let i=0; i<house.swornMembers.length; i++){  
+            service.getSingle(house.swornMembers[i], function(answer) {
+                if (answer !== null) {
+                    self.characters.push(answer);
+                }
+            });
+        }
+    }
+
+    $scope.filterByBook = function(book){
+        self.characters = [];
+        for (let i=0; i<book.characters.length; i++){  
+            service.getSingle(book.characters[i], function(answer) {
+                if (answer !== null) {
+                    self.characters.push(answer);
+                }
+            });
+        }
+    }
+
+    $scope.backToAll = function(){
+        self.characters = self.allCharacters;
+    }
 }]);
+
 

173720d3eaaed40d0fe22cfc171e79af7fb3b4e4
WillianV1tor <willianv1tor.dev@gmail.com>
2018-11-18 03:11:54 -0200

    Favorite - Incomplete

--- app.js
+++ app.js
@@ -1,79 +1,87 @@
 
-var app = angular.module('got', ['ngRoute']);
+var app = angular.module('got', ['ngRoute', 'ngCookies']);
 
 /* Configuração das Rotas */
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "main.html"
-    })
-    .when("/houses", {
-        templateUrl : "houses.html",
-        controller: "Controller",
-        controllerAs: "ctrl" 
-    })
-    .when("/characters", {
-        templateUrl : "characters.html",
-        controller: "Controller",
-        controllerAs: "ctrl" 
-    })
-    .when("/books", {
-        templateUrl : "books.html",
-        controller: "Controller",
-        controllerAs: "ctrl" 
-    });
+        .when("/", {
+            templateUrl: "main.html"
+        })
+        .when("/houses", {
+            templateUrl: "houses.html",
+            controller: "Controller",
+            controllerAs: "ctrl"
+        })
+        .when("/characters", {
+            templateUrl: "characters.html",
+            controller: "Controller",
+            controllerAs: "ctrl"
+        })
+        .when("/books", {
+            templateUrl: "books.html",
+            controller: "Controller",
+            controllerAs: "ctrl"
+        })
+        .when("/favorites", {
+            templateUrl: "favorites.html",
+            controller: "Controller",
+            controllerAs: "ctrl"
+        });
 });
 
 
 /* Service  que faz as requisições */
-app.factory('InfoService', function($http){
-    var service = {};   
+app.factory('InfoService', function ($http) {
+    var service = {};
 
-    service.getMultiple = function(name, page, callback) {
-        $http.get('https://anapioficeandfire.com/api/'+name+'?pageSize=200&page=' + page).then(
-            function(response){callback(response.data);},
-            function(response){callback(null);}
-        )};
-  
-    service.getSingle = function(url, callback) {
+    service.getMultiple = function (name, page, callback) {
+        $http.get('https://anapioficeandfire.com/api/' + name + '?pageSize=200&page=' + page).then(
+            function (response) { callback(response.data); },
+            function (response) { callback(null); }
+        )
+    };
+
+    service.getSingle = function (url, callback) {
         $http.get(url).then(
-            function(response){callback(response.data);},
-            function(response){callback(null);}
-        )};
- 
-  return service;
+            function (response) { callback(response.data); },
+            function (response) { callback(null); }
+        )
+    };
+
+    return service;
 });
 
 
 /* Controller */
-app.controller('Controller', ['$scope', 'InfoService', function($scope, service){
-  var self = this;
-  self.characters = [];
-  self.character = {};
-  self.allCharacters = [];
-    
-  /* Família do personagem */
-  self.father = {};
-  self.mother = {};
-  self.spouse = {};
+app.controller('Controller', ['$scope', 'InfoService', function ($scope, service) {
+    var self = this;
+    self.characters = [];
+    self.character = {};
+    self.allCharacters = [];
+
+    /* Família do personagem */
+    self.father = {};
+    self.mother = {};
+    self.spouse = {};
+    self.url;
 
 
-  self.houses = [];
-  self.house = {};
+    self.houses = [];
+    self.house = {};
 
-  /* Lord atual e ramos mais jovens */ 
-  self.currentLord = {};
-  self.cadetBranches = [];
+    /* Lord atual e ramos mais jovens */
+    self.currentLord = {};
+    self.cadetBranches = [];
 
 
-  self.books = [];
-  self.book = {};
+    self.books = [];
+    self.book = {};
 
 
 
     var page = 1;
-    while (page < 200){ // Recupera todas os personagens
-        service.getMultiple('characters', page, function(answer) {
+    while (page < 200) { // Recupera todas os personagens
+        service.getMultiple('characters', page, function (answer) {
             if (answer !== null) {
                 self.allCharacters = self.allCharacters.concat(answer);
                 self.characters = self.allCharacters;
@@ -84,18 +92,18 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
 
 
     page = 1;
-    while (page < 200){
-        service.getMultiple('houses', page, function(answer) { // Recupera todas as casas
+    while (page < 200) {
+        service.getMultiple('houses', page, function (answer) { // Recupera todas as casas
             if (answer !== null) {
                 self.houses = self.houses.concat(answer);
             }
         });
         page++;
     }
-    
+
     page = 1;
-    while (page < 200){
-        service.getMultiple('books', page, function(answer) { // Recupera todos os livros
+    while (page < 200) {
+        service.getMultiple('books', page, function (answer) { // Recupera todos os livros
             if (answer !== null) {
                 self.books = self.books.concat(answer);
             }
@@ -107,50 +115,50 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
 
 
     // Recupera um personagem
-  self.getCharacter = function(url) {
-    service.getSingle(url, function(answer) {
-      if (answer !== null) {
-        self.character = answer;
-        service.getSingle(self.character.spouse, function(answer) {
+    self.getCharacter = function (url) {
+        service.getSingle(url, function (answer) {
             if (answer !== null) {
-              self.spouse = answer;
-              }
-          });
-        
-        service.getSingle(self.character.mother, function(answer) {
-        if (answer !== null) {
-            self.mother = answer;
-            }
-        });
+                self.character = answer;
+                service.getSingle(self.character.spouse, function (answer) {
+                    if (answer !== null) {
+                        self.spouse = answer;
+                    }
+                });
 
-        service.getSingle(self.character.father, function(answer) {
-        if (answer !== null) {
-            self.father = answer;
+                service.getSingle(self.character.mother, function (answer) {
+                    if (answer !== null) {
+                        self.mother = answer;
+                    }
+                });
+
+                service.getSingle(self.character.father, function (answer) {
+                    if (answer !== null) {
+                        self.father = answer;
+                    }
+                });
+
+                /* Volta para o topo */
+                document.body.scrollTop = 0; // For Safari
+                document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
             }
         });
-        
-        /* Volta para o topo */
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      }
-    });
-  }
+    }
 
 
     // Recupera uma casa específica
-    self.getHouse = function(url) {
-        service.getSingle(url, function(answer) {
+    self.getHouse = function (url) {
+        service.getSingle(url, function (answer) {
             if (answer !== null) {
                 self.house = answer;
             }
-            service.getSingle(self.house.currentLord, function(answer) {
+            service.getSingle(self.house.currentLord, function (answer) {
                 if (answer !== null) {
-                  self.currentLord = answer;
-                  }
-              });
-           
-            for (let i=0; i<self.house.cadetBranches.length; i++){  
-                service.getSingle(self.house.cadetBranches[i], function(answer) {
+                    self.currentLord = answer;
+                }
+            });
+
+            for (let i = 0; i < self.house.cadetBranches.length; i++) {
+                service.getSingle(self.house.cadetBranches[i], function (answer) {
                     if (answer !== null) {
                         self.cadetBranches = self.cadetBranches.concat(answer);
                     }
@@ -158,14 +166,14 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
             }
         });
 
-         /* Volta para o topo */
-         document.body.scrollTop = 0; // For Safari
-         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        /* Volta para o topo */
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
     // Recupera um livro específico
-    self.getBook = function(url) {
-        service.getSingle(url, function(answer){
+    self.getBook = function (url) {
+        service.getSingle(url, function (answer) {
             if (answer !== null) {
                 self.book = answer;
             }
@@ -176,10 +184,10 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
-    $scope.filterByHouse = function(house){
+    $scope.filterByHouse = function (house) {
         self.characters = [];
-        for (let i=0; i<house.swornMembers.length; i++){  
-            service.getSingle(house.swornMembers[i], function(answer) {
+        for (let i = 0; i < house.swornMembers.length; i++) {
+            service.getSingle(house.swornMembers[i], function (answer) {
                 if (answer !== null) {
                     self.characters.push(answer);
                 }
@@ -187,10 +195,10 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
         }
     }
 
-    $scope.filterByBook = function(book){
+    $scope.filterByBook = function (book) {
         self.characters = [];
-        for (let i=0; i<book.characters.length; i++){  
-            service.getSingle(book.characters[i], function(answer) {
+        for (let i = 0; i < book.characters.length; i++) {
+            service.getSingle(book.characters[i], function (answer) {
                 if (answer !== null) {
                     self.characters.push(answer);
                 }
@@ -198,9 +206,28 @@ app.controller('Controller', ['$scope', 'InfoService', function($scope, service)
         }
     }
 
-    $scope.backToAll = function(){
+    $scope.backToAll = function () {
         self.characters = self.allCharacters;
     }
 }]);
 
 
+app.controller('CookiesController', ['$scope', '$cookies', function ($scope, $cookies) {
+    $scope.SetCookies = function (att) {
+        var arrayChar = [];
+
+        if ($cookies.get('urlChar') !== 'undefined') {
+            arrayChar.push($cookies.get('urlChar'));
+        }
+        arrayChar.push(att);
+
+        console.log(arrayChar);
+        $cookies.put("urlChar", arrayChar);
+    };
+    $scope.GetCookies = function () {
+        return $cookies.get('urlChar');
+    };
+    $scope.ClearCookies = function () {
+        $cookies.remove('urlChar');
+    };
+}]);

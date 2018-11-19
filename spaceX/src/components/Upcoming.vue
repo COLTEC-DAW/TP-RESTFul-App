74d34957d63d8e23e3e8c87e6dfb97aaafbf6ff4
ibryans <bryans0701@gmail.com>
2018-11-19 10:12:04 -0200

    Consertando erros de conflito

--- spaceX/src/components/Upcoming.vue
+++ spaceX/src/components/Upcoming.vue
@@ -1,34 +1,11 @@
 <template>
     <div class="columns upcoming">
         <section class="p-5 column is-6 has-text-centered">
-<<<<<<< HEAD
-            <h1 class="subtitle">Next launch from NASA</h1>
-            <div>
-                <div class="day">
-                    <span class="number">{{ days }}</span>
-                    <div class="format">{{ wordString.day }}</div>
-                </div>
-                <div class="hour">
-                    <span class="number">{{ hours }}</span>
-                    <div class="format">{{ wordString.hours }}</div>
-                </div>
-                <div class="min">
-                    <span class="number">{{ minutes }}</span>
-                    <div class="format">{{ wordString.minutes }}</div>
-                </div>
-                <div class="sec">
-                    <span class="number">{{ seconds }}</span>
-                    <div class="format">{{ wordString.seconds }}</div>
-                </div>
-                <div class="message">{{ message }}</div>
-                <div class="status-tag" :class="statusType">{{ statusText }}</div>
-=======
             <div class="card">
               <div class="card-content">
                 <h1 class="subtitle">Next launch from NASA</h1>
-                <h1 id="count" class="title"></h1>
+                <h1 class="title">{{day}}d {{hours}}h {{minutes}}m {{seconds}}s</h1>
               </div>
->>>>>>> Estilo do header e do upcoming
             </div>
         </section>
         <section id="launch_info" class="p-5 is-6" v-for="upcoming in upcoming" :key="upcoming">
@@ -97,7 +74,6 @@ export default {
         this.upcoming = response.data
       })
     },
-<<<<<<< HEAD
     timerCount: function (start, end) {
       // Get todays date and time
       var now = new Date().getTime()
@@ -121,54 +97,6 @@ export default {
         this.message = this.wordString.upcoming
         this.statusType = 'upcoming'
         this.statusText = this.wordString.status.upcoming
-=======
-
-    methods: {
-      getUpcomingLaunchData: function () {
-        this.$http.get('https://api.spacexdata.com/v3/launches/upcoming/?flight_number=72').then((response) => {
-          this.upcoming = response.data
-        })
-      },
-      createCountDown: function() {
-          var countDownDate = new Date("Dec 4, 2018 16:38:00").getTime();
-
-          // Update the count down every 1 second
-          var x = setInterval(function() {
-
-            // Get todays date and time
-            var now = new Date().getTime();
-
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-            console.log(countDownDate, distance, now)
-
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            // Display the result in the element with id="demo"
-            document.getElementById("count").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-
-            // If the count down is finished, write some text 
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("count").innerHTML = "EXPIRED";
-            }
-          }, 1000);
-      },
-      parseDate: function (date) {
-        var dateParsed = new Date(date).toLocaleDateString('pt-BR', {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric'
-        })
-        return dateParsed  
->>>>>>> Estilo do header e do upcoming
       }
     },
     parseDate: function (date) {
@@ -207,6 +135,3 @@ export default {
     background-color: rgb(202, 202, 202);
 }
 </style>
-// Background imagem de uma estaçao de foguete sei la
-// Cards para o coutdown e as informações
-// Pegar todos os upcomings e so mostrar o que a date é maior que a atual

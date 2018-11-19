556aae55ab145e5ba64b84008378884d5e17b730
Bernardo Nunes <nunesbernardo2000@gmail.com>
2018-11-19 08:19:32 -0200

    Codigo bonitao

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ app.config(function ($routeProvider) {
         })
         .when("/houses", {
             templateUrl: "houses.html",
-            controller: "Controller",
+            controller: "ControllerHouses",
             controllerAs: "ctrl"
         })
         .when("/characters", {
             templateUrl: "characters.html",
-            controller: "Controller",
+            controller: "ControllerChars",
             controllerAs: "ctrl"
         })
         .when("/books", {
             templateUrl: "books.html",
-            controller: "Controller",
+            controller: "ControllerBooks",
             controllerAs: "ctrl"
         })
         .when("/favorites", {
             templateUrl: "favorites.html",
-            controller: "Controller",
+            controller: "ControllerFavorites",
             controllerAs: "ctrl"
         });
 });
@@ -51,13 +51,31 @@ app.factory('InfoService', function ($http) {
     return service;
 });
 
+    
+/* Recupera um cookie (função retirada do w3schools) */
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-/* Controller */
-app.controller('Controller', ['$scope', 'InfoService', function ($scope, service) {
+
+/* Controller Personagens */
+app.controller('ControllerChars', ['$scope', 'InfoService', function ($scope, service) {
     var self = this;
-    self.characters = [];
-    self.character = {};
-    self.allCharacters = [];
+    self.characters = []; // Personagens filtrados
+    self.character = {}; // Personagem escolhido
+    self.allCharacters = []; // Todos os personagens
 
     /* Família do personagem */
     self.father = {};
@@ -65,21 +83,13 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
     self.spouse = {};
     self.url;
 
-
     self.houses = [];
-    self.house = {};
-
-    /* Lord atual e ramos mais jovens */
-    self.currentLord = {};
-    self.cadetBranches = [];
-
-
     self.books = [];
-    self.book = {};
 
 
+    /* Recupera todas os personagens, casase livros */
     var page = 1;
-    while (page < 200) { // Recupera todas os personagens
+    while (page < 200) { 
         service.getMultiple('characters', page, function (answer) {
             if (answer !== null) {
                 self.allCharacters = self.allCharacters.concat(answer);
@@ -89,20 +99,11 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
         page++;
     }
 
-
+    
+    /* Recupera todos os livros */
     page = 1;
-    while (page < 200) {
-        service.getMultiple('houses', page, function (answer) { // Recupera todas as casas
-            if (answer !== null) {
-                self.houses = self.houses.concat(answer);
-            }
-        });
-        page++;
-    }
-
-    page = 1;
-    while (page < 200) {
-        service.getMultiple('books', page, function (answer) { // Recupera todos os livros
+    while (page < 2) {
+        service.getMultiple('books', page, function (answer) {
             if (answer !== null) {
                 self.books = self.books.concat(answer);
             }
@@ -111,33 +112,35 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
     }
 
 
+    /* Recupera todos os livros */
+    page = 1;
+    while (page < 200) {
+        service.getMultiple('houses', page, function (answer) {
+            if (answer !== null) {
+                self.houses = self.houses.concat(answer);
+            }
+        });
+        page++;
+    }
+
+
+
+    /* Adiciona estrela nos personagens favoritos */
     $(document).ready(function () {
-        /* Adiciona checked em favoritos */
-        var urls = JSON.parse(getCookie('characters'));
-        for (let i=0; i<urls.length; i++){
-            service.getSingle(urls[i], function (answer) {
-                if (document.getElementById(answer.url) != null) {
-                    document.getElementById(answer.url).setAttribute('checked', '');
-                }
-            });
-        }
-        var urls = JSON.parse(getCookie('houses'));
-        for (let i=0; i<urls.length; i++){
-            service.getSingle(urls[i], function (answer) {
-                if (document.getElementById(answer.url) != null) {
-                    document.getElementById(answer.url).setAttribute('checked', '');
-                }
-            });
-        }
-        var urls = JSON.parse(getCookie('books'));
-        for (let i=0; i<urls.length; i++){
-            service.getSingle(urls[i], function (answer) {
-                if (document.getElementById(answer.url) != null) {
-                    document.getElementById(answer.url).setAttribute('checked', '');
-                }
-            });
+        var characters = getCookie('characters');
+        console.log('haha');
+        if (characters != ''){
+            var urls = JSON.parse(characters);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
         }
     });
+
 
     // Recupera um personagem
     self.getCharacter = function (url) {
@@ -169,46 +172,6 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
         });
     }
 
-
-    // Recupera uma casa específica
-    self.getHouse = function (url) {
-        service.getSingle(url, function (answer) {
-            if (answer !== null) {
-                self.house = answer;
-            }
-            service.getSingle(self.house.currentLord, function (answer) {
-                if (answer !== null) {
-                    self.currentLord = answer;
-                }
-            });
-
-            for (let i = 0; i < self.house.cadetBranches.length; i++) {
-                service.getSingle(self.house.cadetBranches[i], function (answer) {
-                    if (answer !== null) {
-                        self.cadetBranches = self.cadetBranches.concat(answer);
-                    }
-                });
-            }
-        });
-
-        /* Volta para o topo */
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-
-    // Recupera um livro específico
-    self.getBook = function (url) {
-        service.getSingle(url, function (answer) {
-            if (answer !== null) {
-                self.book = answer;
-            }
-        });
-
-        /* Volta para o topo */
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-
     // Filtra personagens pela casa
     $scope.filterByHouse = function (house) {
         self.characters = [];
@@ -237,24 +200,272 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
     $scope.backToAll = function () {
         self.characters = self.allCharacters;
     }
+        
+    /* Adiciona e deleta casas dos favoritos */
+    self.cookie = function(url){
+        var cookies = getCookie('characters');
 
+        if (cookies.includes(url)){
+            cookies = JSON.parse(cookies);
+            cookies.splice(cookies.indexOf(url), 1);
+            
+        }else{
+            if (cookies == ''){
+                cookies = '[]';
+            }
+            cookies = JSON.parse(cookies);
+            cookies.push(url);
+        }
+        document.cookie= 'characters='+JSON.stringify(cookies);
+        location.reload();
+    }
+
+}]);
+
+/* Controller Livros */
+app.controller('ControllerBooks', ['InfoService', function (service) {
+    var self = this;
+    self.books = []; // Todos os livros
+    self.book = {}; // Livros selecionados
+
+    /* Recupera todos os livros */
+    page = 1;
+    while (page < 2) {
+        service.getMultiple('books', page, function (answer) {
+            if (answer !== null) {
+                self.books = self.books.concat(answer);
+                service.books = self.books;
+            }
+        });
+        page++;
+    }
+
+     /* Adiciona estrela em livros favoritos */
+     $(document).ready(function () {
+        var books = getCookie('books');
+        if (books != ''){
+            var urls = JSON.parse(books);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
+        }
+    });
+
+    /* Recupera um livro específico */
+    self.getBook = function (url) {
+        service.getSingle(url, function (answer) {
+            if (answer !== null) {
+                self.book = answer;
+            }
+        });
+
+        /* Volta para o topo */
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    /* Adiciona e deleta livros dos favoritos */
+    self.cookie = function(url){
+        var cookies = getCookie('books');
     
-    // Retorna os favoritos
-    this.getFavorites = function(tipo){
-        var itens = [];
-        var urls = JSON.parse(getCookie(tipo));
+        if (cookies.includes(url)){
+            cookies = JSON.parse(cookies);
+            cookies.splice(cookies.indexOf(url), 1);
+            
+        }else{
+            if (cookies == ''){
+                cookies = '[]';
+            }
+            cookies = JSON.parse(cookies);
+            cookies.push(url);
+        }
+        document.cookie= 'books='+JSON.stringify(cookies);
+        location.reload();
+    }
+
+}]);
+
+
+/* Controller Casas */
+app.controller('ControllerHouses', ['InfoService', function (service) {
+    var self = this;
+    self.houses = []; // Todas as casas
+    self.house = {}; // Casa selecionada
+
+    /* Lord atual e ramos mais jovens */
+    self.currentLord = {};
+    self.cadetBranches = [];
+
+
+    /* Recupera todas as casas */
+    page = 1;
+    while (page < 200) {
+        service.getMultiple('houses', page, function (answer) { 
+            if (answer !== null) {
+                self.houses = self.houses.concat(answer);
+                service.houses = self.houses;
+            }
+        });
+        page++;
+    }
+
+    /* Adiciona estrela em casas favoritas */
+    $(document).ready(function () {
+        var houses = getCookie('houses');
+        if (houses != ''){
+            var urls = JSON.parse(houses);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
+        }
+    });
+
+
+    /* Recupera uma casa específica */
+    self.getHouse = function (url) {
+        service.getSingle(url, function (answer) {
+            if (answer !== null) {
+                self.house = answer;
+            }
+            service.getSingle(self.house.currentLord, function (answer) {
+                if (answer !== null) {
+                    self.currentLord = answer;
+                }
+            });
+
+            for (let i = 0; i < self.house.cadetBranches.length; i++) {
+                service.getSingle(self.house.cadetBranches[i], function (answer) {
+                    if (answer !== null) {
+                        self.cadetBranches = self.cadetBranches.concat(answer);
+                    }
+                });
+            }
+        });
+
+        /* Volta para o topo */
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    /* Adiciona e deleta casas dos favoritos */
+    self.cookie = function(url){
+        var cookies = getCookie('houses');
+    
+        if (cookies.includes(url)){
+            cookies = JSON.parse(cookies);
+            cookies.splice(cookies.indexOf(url), 1);
+            
+        }else{
+            if (cookies == ''){
+                cookies = '[]';
+            }
+            cookies = JSON.parse(cookies);
+            cookies.push(url);
+        }
+        document.cookie= 'houses='+JSON.stringify(cookies);
+        location.reload();
+    }
+}]);
+
+
+app.controller('ControllerFavorites', ['InfoService', function (service) {
+    self = this;
+    self.characters = [];
+    self.houses = [];
+    self.books = [];
+    
+    favoritos = getCookie('characters');
+    if (favoritos != ''){
+        var urls = JSON.parse(favoritos);
         for (let i=0; i<urls.length; i++){
             service.getSingle(urls[i], function (answer) {
                 if (answer !== null) {
-                    itens.push(answer);
+                    self.characters.push(answer);
                 }
             });
         }
-        return itens;
     }
 
-    // Cria e deleta cookies //
-    this.cookie = function(url){
+    favoritos = getCookie('houses');
+    if (favoritos != ''){
+        var urls = JSON.parse(favoritos);
+        for (let i=0; i<urls.length; i++){
+            service.getSingle(urls[i], function (answer) {
+                if (answer !== null) {
+                    self.houses.push(answer);
+                }
+            });
+        }
+    }
+
+    favoritos = getCookie('books');
+    if (favoritos != ''){
+        var urls = JSON.parse(favoritos);
+        for (let i=0; i<urls.length; i++){
+            service.getSingle(urls[i], function (answer) {
+                if (answer !== null) {
+                    self.books.push(answer);
+                }
+            });
+        }
+    }
+
+    /* Adiciona estrela em casas favoritas */
+    $(document).ready(function () {
+        var houses = getCookie('characters');
+        if (houses != ''){
+            var urls = JSON.parse(houses);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
+        }
+    });
+
+    /* Adiciona estrela em casas favoritas */
+    $(document).ready(function () {
+        var houses = getCookie('houses');
+        if (houses != ''){
+            var urls = JSON.parse(houses);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
+        }
+    });
+
+
+    /* Adiciona estrela em casas favoritas */
+    $(document).ready(function () {
+        var houses = getCookie('books');
+        if (houses != ''){
+            var urls = JSON.parse(houses);
+            for (let i=0; i<urls.length; i++){
+                service.getSingle(urls[i], function (answer) {
+                    if (document.getElementById(answer.url) != null) {
+                        document.getElementById(answer.url).setAttribute('checked', '');
+                    }
+                });
+            }
+        }
+    });
+
+
+    self.cookie = function(url){
         var cookies = null;
         var tipo = '';
         if (url.includes('characters')){
@@ -264,7 +475,7 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
             cookies = getCookie('houses');
             tipo = 'houses';
         }else if (url.includes('books')){
-            cookies = getCookie('houses');
+            cookies = getCookie('books');
             tipo = 'books';
         }      
 
@@ -282,24 +493,7 @@ app.controller('Controller', ['$scope', 'InfoService', function ($scope, service
         document.cookie= tipo+'='+JSON.stringify(cookies);
         location.reload();
     }
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
 }]);
-
 /*
 app.controller('CookiesController', ['$scope', '$cookies', function ($scope, $cookies) {
     $scope.SetCookies = function (att) {

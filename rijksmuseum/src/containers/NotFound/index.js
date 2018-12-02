b86b319cd001af69ce28d2ff8d1f302ba7046c69
RitaRez <ritarezborges@gmail.com>
2018-12-02 11:37:27 -0200

    tes

--- rijksmuseum/src/containers/NotFound/index.js
+++ rijksmuseum/src/containers/NotFound/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
 function NotFound (vacancies) {
-
-
   return (
     <div>Uh, seems like theres nothing here, perhaps youre looking for one those lost paintings.</div>
   )
-
 }
 export default NotFound

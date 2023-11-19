21b324c68db5f2ab4819f991a98aea24d7e6fba1
Inaue Ferreira da Silva <inaue.silva@gmail.com>
2023-11-19 15:50:54 -0300

    Creating basic conversion for currencies.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
+const api_url = 'https://api.frankfurter.app/'
+
 const converter = () =>
 {
-    //code
+    const num  = $('#num_ref').val()
+    const ref  = $('#moeda_ref').val()
+    const conv = $('#moeda_conv').val()
+
+    if(num != '' && ref != '' && conv != '')
+    {
+        if(ref == conv)
+        {
+            $('#num_conv').text(parseFloat(num).toFixed(2))
+        }
+        else
+        {
+            $.get(api_url + 'latest?amount=' + num + '&from=' + ref + '&to=' + conv)
+            .done(res =>
+                $('#num_conv').text(parseFloat(res.rates[conv]).toFixed(2))
+            )
+        }
+    }
+    else
+    {
+        $('#num_conv').text(0.00.toFixed(2))
+    }
 }
 
 let current_page = $('#pagina_principal')
@@ -19,6 +42,30 @@ $('#btn_hj').on('click', () =>
     current_page.show()
 })
 
+$('#num_ref').on('change', converter)
+
 $('#moeda_ref').on('change', converter)
 
 $('#moeda_conv').on('change', converter)
+
+$.get(api_url + 'currencies')
+.done(res =>
+{
+    let option
+
+    Object.keys(res).forEach(c =>
+    {
+        option = '<option value="' + c + '">'
+        option += c + ' (' + res[c] + ')'
+        option += '</option>'
+        $('#moeda_ref').append(option)
+        $('#moeda_conv').append(option)
+    });
+})
+.fail(erro =>
+{
+    const option = '<option value="">Opções Indisponíveis</option>'
+    
+    $('#moeda_ref').append(option)
+    $('#moeda_conv').append(option)
+})

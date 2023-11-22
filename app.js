0a91f6363ea5e4a905c791fc1175268ee8726f2d
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-11-22 16:23:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+function listarRecursos(){
+    document.getElementById("resources").style = "display: grid;";
+    document.getElementById("resources").innerText = " ";
+    $.ajax({
+        url: 'http://data.fixer.io/api/latest?access_key=d4148233556a36282035fa1e5ea85c92',
+        dataType: 'json',
+        success: function (data) {
+            // Manipule os dados conforme necessário para listar os recursos desejados
+            var resources = Object.keys(data);
+    
+            // Exiba os recursos na sua página HTML
+            var resourcesList = $('#resources');
+            resources.forEach(function (resource) {
+                resourcesList.append('<li><button class="butRes" onclick="buscarRecurso(this.innerText);">' + resource + '</button></li>');
+            });
+        },
+        error: function () {
+            $('#error').show();
+            $('#resources').hide();
+        },
+        complete: function () {
+            console.log("request completed");
+        }
+    });
+}
+
+function buscarRecurso(recurso) {
+    if(recurso == null){
+        recurso = document.getElementById("recurso").value;
+    }
+    document.getElementById("resources").style = "display: none;";
+    $.ajax({
+        url: 'http://data.fixer.io/api/latest?access_key=d4148233556a36282035fa1e5ea85c92',
+        dataType: 'json',
+        success: function (data) {
+            // Verifica se o recurso fornecido existe nos dados
+            $('resources').hide();
+            if (data[recurso] !== undefined) {
+                // Exibe o recurso na sua página HTML
+                $('#result').html('<p style="max-width: 300;">' + recurso + ': ' + JSON.stringify(data[recurso], null, 2) + '</p>');
+                $('#error').hide();
+            } else {
+                // Se o recurso não for encontrado, exibe uma mensagem de erro
+                $('#result').empty();
+                $('#error').show().text('Recurso não encontrado');
+            }
+        },
+        error: function () {
+            $('#error').show().text('Erro na requisição');
+            $('#result').empty();
+        },
+        complete: function () {
+            console.log("request completed");
+        }
+    });
+}
+
+
+
+

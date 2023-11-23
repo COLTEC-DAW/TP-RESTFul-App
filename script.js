b0a61e16c6514c1b9c3c56400ba512bbc7450af0
Daniel Prates <danielaugustoprates@gmail.com>
2023-11-23 15:41:47 -0300

    Acessando dados especificos

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-fetch('https://content.guardianapis.com/search?page=2&q=debate&api-key=test').then((res) => res.json())
-.then((data) => console.log(data))
+fetch('https://content.guardianapis.com/search?page=2&q=debate&api-key=test')
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(JSON.stringify(data.response.results, null, 2));
+
+  })
+  .catch((error) => {
+    console.error('Erro ao recuperar os dados:', error);
+  });

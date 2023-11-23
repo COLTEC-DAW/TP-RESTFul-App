662b0ba375bddd7dd6dfc2c75ad90ff0d9a772ee
ArthurMacielSs <104994335+ArthurMacielSs@users.noreply.github.com>
2023-11-23 15:44:25 -0300

    adicionando prototipo do trabalho

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-fetch('https://content.guardianapis.com/search?page=2&q=debate&api-key=test').then((res) => res.json())
+fetch('https://content.guardianapis.com/search?page=2&q=debate&api-key=18f36e1a-de90-4893-b549-64d800c3fd9b').then((res) => res.json())
 .then((data) => console.log(data))
